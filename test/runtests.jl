16d2b0f55774358a9bb80a005c50aabd3747a664
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-09-16 18:04:12 -0700

    suggested next steps in comments in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ include("sample_data.jl")
 
 
 function test_truth_most_likely_with_all_synthetic_data()
-    const test_img = generate_sample_image()
+    test_img = generate_sample_image()
 
     # it's a pretty bright asteroid, so even with the psf,
     # the pixel that contains the asteroid should be the brightest
@@ -13,10 +13,10 @@ function test_truth_most_likely_with_all_synthetic_data()
 
     prior = sample_prior()
 
-    const good_ast = AsteroidParams(10., [20, 12.], [3.1, 5.1])
+    good_ast = AsteroidParams(10., [20, 12.], [3.1, 5.1])
     good_ll = compute_log_probability(good_ast, test_img, prior)
 
-    const bad_ast = AsteroidParams(10., [19.4, 12.], [3.1, 5.1])
+    bad_ast = AsteroidParams(10., [19.4, 12.], [3.1, 5.1])
     bad_ll = compute_log_probability(bad_ast, test_img, prior)
 
     info("$good_ll > $bad_ll")
@@ -27,17 +27,16 @@ end
 function test_truth_most_likely_with_wise_psf()
     band_id = 3
     halfsidelen = 2
-
     psf = load_wise_psf(band_id, halfsidelen) # sidelength will be 2*2 + 1
     psf /= sum(psf)
 
-    const test_img = generate_sample_image(psf)
+    test_img = generate_sample_image(psf)
     prior = sample_prior()
 
-    const good_ast = AsteroidParams(10., [20, 12.], [3.1, 5.1])
+    good_ast = AsteroidParams(10., [20, 12.], [3.1, 5.1])
     good_ll = compute_log_probability(good_ast, test_img, prior)
 
-    const bad_ast = AsteroidParams(10., [19.4, 12.], [3.1, 5.1])
+    bad_ast = AsteroidParams(10., [19.4, 12.], [3.1, 5.1])
     bad_ll = compute_log_probability(bad_ast, test_img, prior)
 
     info("$good_ll > $bad_ll")
@@ -45,9 +44,30 @@ function test_truth_most_likely_with_wise_psf()
 end
 
 
+# thats the actual path for asteroid 2005_UT453 has the highest
+# probability according to our model
 function test_truth_most_likely_with_all_real_data()
-    # TODO: test that the actual path for asteroid 2005_UT453 has the highest
-    # probability according to our model
+    band_id = 2
+    halfsidelen = 2
+    psf = load_wise_psf(band_id, halfsidelen)
+    psf /= sum(psf)
+    @test(minimum(psf) > 0)
+
+    # Possible steps to writing this test:
+    #
+    # 1. image_stack = <load images of 2005_UT453 taken in band 2>
+    # 2. good_ast = <load the true band 2 brigthness of the 2005_UT453,
+    #           its true position (in pixel coordinates at time 0)
+    #           and its true velocity during the image exposures>
+    #           Note: time 0 can be time of the first image in the stack
+    # 3. good_ll compute_log_prob(good_ast, image_stack, prior), where
+    #            prior is set like it's done in sample_prior(), but with
+    #            more realistic values
+    # 4. for a variety of bad_ast, some perhaps not all that different
+    #    from good_ast, do 
+    #             bad_ll = compute_log_prob(bad_ast, image_stack, prior)
+    #    and
+    #          @test bad_ll < good_ll
 end
 
 
