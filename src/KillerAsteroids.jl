45c6ccfc27fa6d86ff8fe85ff71380c78f1d0188
Aaron Meisner <ameisner@fas.harvard.edu>
2015-10-01 14:21:43 -0700

    starting WISE utilities, extracting special numbers

--- src/KillerAsteroids.jl
+++ src/KillerAsteroids.jl
@@ -7,9 +7,11 @@ using Distributions
 include("model_types.jl")
 include("model_probability.jl")
 include("wise_psf.jl")
+include("wise_utils.jl")
 
 export Image, AsteroidParams, Prior
 export compute_log_probability, compute_log_likelihood
 export load_wise_psf
+export wise_params
 
 end # module
