a8e11e53d487f96a881e2d8c9a50daf40037bc2f
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-09-30 17:54:10 -0700

    computes the probability of 0 or more asteroids

--- src/model_probability.jl
+++ src/model_probability.jl
@@ -1,6 +1,6 @@
 
-function compute_log_prior(ast::AsteroidParams, prior::Prior)
-    logpdf(prior.r, ast.r) + logpdf(prior.v, ast.v)
+function compute_log_prior(asteroids::Vector{AsteroidParams}, prior::Prior)
+    sum([logpdf(prior.r, ast.r) + logpdf(prior.v, ast.v) for ast in asteroids])
 end
 
 """
@@ -16,28 +16,38 @@ function extrapolate_position(u0::Vector{Float64}, v::Vector{Float64}, t::Float6
     u0 + v * t 
 end
 
-function compute_log_likelihood(ast::AsteroidParams, img::Image)
+function compute_log_likelihood(asteroids::Vector{AsteroidParams},
+        images::Vector{Image})
     ll = 0.
-    psf_dims = size(img.psf)
-    psf_center = [round(Int, (dim + 1) / 2) for dim in size(img.psf)]
-    u_t = extrapolate_position(ast.u, ast.v, img.t)
-    u_t_px = round(Int, u_t)
-    for w2 in 1:psf_dims[2], h2 in 1:psf_dims[1]
-        h = u_t_px[1] + h2 - psf_center[1]
-        w = u_t_px[2] + w2 - psf_center[2]
-        expected_ast_dn = (ast.r * img.psf[h2, w2]) / img.nmgy_per_dn
-        expected_pixel_dn = img.sky_noise_mean + expected_ast_dn
-        pixel_dn_var = expected_pixel_dn + img.read_noise_var
-        pixel_dist = Normal(expected_pixel_dn, sqrt(pixel_dn_var))
-        ll += logpdf(pixel_dist, img.pixels[h, w])
+
+    for img in images
+        psf_dims = size(img.psf)
+        psf_center = [round(Int, (dim + 1) / 2) for dim in size(img.psf)]
+        expected_dn = similar(img.pixels)
+        fill!(expected_dn, img.sky_noise_mean)
+
+        for ast in asteroids
+            u_t = extrapolate_position(ast.u, ast.v, img.t)
+            u_t_px = round(Int, u_t)
+            ast_r_dn = ast.r / img.nmgy_per_dn
+            for w2 in 1:psf_dims[2], h2 in 1:psf_dims[1]
+                h = u_t_px[1] + h2 - psf_center[1]
+                w = u_t_px[2] + w2 - psf_center[2]
+                expected_ast_dn = ast_r_dn * img.psf[h2, w2]
+                expected_dn[h, w] += expected_ast_dn
+            end
+        end
+
+        for w in 1:img.W, h in 1:img.H
+            pixel_dn_var = expected_dn[h, w] + img.read_noise_var
+            pixel_dist = Normal(expected_dn[h, w], sqrt(pixel_dn_var))
+            ll += logpdf(pixel_dist, img.pixels[h, w])
+        end
     end
+
     ll
 end
 
-
-function compute_log_likelihood(ast::AsteroidParams, img_stack::Vector{Image})
-    sum([compute_log_likelihood(ast, img) for img in img_stack])
-end
 
 
 """
@@ -48,8 +58,11 @@ arguments:
   ast: parameters for a candidate asteroid
   img: an astronomical image
 """
-function compute_log_probability(ast::AsteroidParams, 
-        img_data::Union(Image, Vector{Image}), prior::Prior)
-    compute_log_prior(ast, prior) + compute_log_likelihood(ast, img_data)
+function compute_log_probability(asteroids::Vector{AsteroidParams},
+        images::Vector{Image}, prior::Prior)
+    lp = compute_log_prior(asteroids, prior)
+    ll = compute_log_likelihood(asteroids, images)
+
+    lp + ll
 end
 
