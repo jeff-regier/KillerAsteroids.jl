77634f449b6560db67e03b39ac583d2b682170e0
Aaron Meisner <ameisner@fas.harvard.edu>
2015-10-07 00:07:15 -0700

    WISE intensity, mask and uncertainty image file names; full example image with unit test

--- src/KillerAsteroids.jl
+++ src/KillerAsteroids.jl
@@ -13,6 +13,7 @@ export Image, AsteroidParams, Prior
 export compute_log_probability, compute_log_likelihood
 export load_wise_psf, extrapolate_position
 export wise_band_to_params
+export l1b_image_name
 export B # number of filter band
 
 end # module
